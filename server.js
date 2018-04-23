7164d0a0852fc370e381b44e63811a73a80f19ef
CuHTeTuk <cuhtetuk@CuHTeTuks-MacBook-Pro.local>
2018-04-22 19:32:38 -0500

    path was updated

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3011;
+var PORT = 3012;
 
+require('./app/routing/htmlRoutes.js')(app);
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.text());
 
 
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
-
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
 
 
 // Starts the server to begin listening
